052ef2a9fe774e175b0846d312557374a97504f8
Dominic Marandino <marandind0@students.rowan.edu>
2018-10-28 21:50:06 -0400

    FIXED BACK
    FIXED BACK

--- app/src/main/java/com/example/user/treasurehunter/PinActivity.java
+++ app/src/main/java/com/example/user/treasurehunter/PinActivity.java
@@ -3,6 +3,7 @@ package com.example.user.treasurehunter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -122,4 +123,18 @@ public class PinActivity extends AppCompatActivity
         mainIntent.putExtra("pin", pin);
         startActivity(mainIntent);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK )
+        {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
